83036dce4041c968cc39d9bac570f81b81f39dad
zacaj <github@zacaj.com>
2012-06-18 16:47:05 -0400

    wc

--- y-exclamation-mark-parser/debugIF.cpp
+++ y-exclamation-mark-parser/debugIF.cpp
@@ -5,6 +5,17 @@ void debugIntermediateForm(FILE *fp)
 	for(int iFunc=0;iFunc<functions.size();iFunc++)
 	{
 		Function *func=functions[iFunc];
+		if(func->ret!=NULL)
+		{
+			fprintf(fp,"r %s ",func->ret->type->name.c_str());
+			if(func->ret->name.empty())
+				fprintf(fp,"NONE ");
+			else
+				fprintf(fp,"%s ",func->ret->name.c_str());
+		}
+		else
+			fprintf(fp,"r void ret");
+
 
 	}
 }
